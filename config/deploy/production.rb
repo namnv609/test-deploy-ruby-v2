74017ae1c56a611687f21efe77e646c3cf876e86
NamNV609 <namnv609@gmail.com>
2017-03-05 17:31:16 +0700

    Change Rails env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ set :port, 2222
 set :user, "deploy"
 set :deploy_via, :remote_cache
 set :conditionally_migrate, true
-set :stage, :production
+set :rails_env, "production"
 
 server "127.0.0.1", user: fetch(:user), port: fetch(:port), roles: %w(web app db)
